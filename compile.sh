bc28e0ad0ca5e9653b3e8a6b297fda7e43f38444
Kkasperek01 <kkasperek0445@gmail.com>
2026-01-22 12:05:00 +0100

    moved comiple from root to scripts

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf build
-mkdir -p build
-
-go build -o build/stock-market-bot ./src/*.go
---
+++ scripts/compile.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Kompilacja projektu Go (przykład)
+REM go build -o ..\bin\stock-market-bot.exe ..\src\main.go
+
+echo Tutaj dodaj polecenie kompilacji po dodaniu kodu.
