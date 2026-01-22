e2a720cdb4b96073f7f6303ebd3cf81b18e6db6a
Kkasperek01 <kkasperek0445@gmail.com>
2026-01-22 12:05:28 +0100

    created test script

---
+++ scripts/test.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Uruchomienie testów Go (przykład)
+REM go test ..\...
+
+echo Tutaj dodaj polecenie uruchomienia testów po dodaniu kodu.
---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Uruchomienie testów Go (przykład)
+# go test ../...
+
+echo "Tutaj dodaj polecenie uruchomienia testów po dodaniu kodu."
